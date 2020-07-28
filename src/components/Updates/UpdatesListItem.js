566320315b804a3da82b99c0fc3987a3861677d5
dytra <vancielnugraha@gmail.com>
2020-07-28 18:03:02 +0700

    add toLocaleString for number terkonfirmasi & sembuh

--- src/components/Updates/UpdatesListItem.js
+++ src/components/Updates/UpdatesListItem.js
@@ -6,7 +6,7 @@ const UpdatesListItem = ({ update }) => {
     <aside className="update-list-item--info">
       <em className="update-list-item--date">{update.reportDateString}</em>
       <div className="update-list-item--numbers">
-        <span className="color-warning"><i className="fas fa-radiation-alt"></i> Terkonfirmasi : {update.totalConfirmed}</span><span className="color-success"> <i className="fas fa-heartbeat"></i> Sembuh: {update.totalRecovered}</span></div>
+        <span className="color-warning"><i className="fas fa-radiation-alt"></i> Terkonfirmasi : {update.totalConfirmed?.toLocaleString('id')}</span><span className="color-success"> <i className="fas fa-heartbeat"></i> Sembuh: {update.totalRecovered?.toLocaleString('id')}</span></div>
       <div className="update-list-item--detail"><p>Total kasus di China : {update.mainlandChina}. Total kasus selain di China : {update.otherLocations}</p></div>
     </aside>
   </li></React.Fragment>)
